154321a82da42ab2211865d3fa46cbb134d1763e
EdilsonMaria <edilson.eng1@gmail.com>
2025-03-20 19:40:37 -0300

    Inserção da aula03

---
+++ aula03/App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import LoginScreen from './src/screens/LoginScreen'
+
+const App = () => {
+  return (
+      <LoginScreen></LoginScreen>
+  )
+}
+
+export default App
