2bb84479532eb44587d37d58daacdea2641a02f6
EdilsonMaria <edilson.eng1@gmail.com>
2025-03-27 19:41:46 -0300

    inserção da LoginScreen

--- aula03/App.js
+++ aula03/App.js
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import LoginScreen from './src/screens/LoginScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import AppNavigation from './src/navigation/AppNavigation'
 
 const App = () => {
   return (
-      <LoginScreen></LoginScreen>
+      <NavigationContainer>
+        <AppNavigation/>
+      </NavigationContainer>
   )
 }
 
---
+++ aula03/src/navigation/AppNavigation.js
@@ -0,0 +1,34 @@
+import { View, Text } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
+import React from 'react'
+import LoginScreen from '../screens/LoginScreen'
+import DatailsScreen from '../screens/DatailsScreen'
+
+const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const MainTabs = () => {
+    return (
+        <Tab.Navigator screenOptions={ { headerShown: false } }>
+            <Tab.Screen 
+                name="Home"
+                component={HomeScreen}
+                options={{ tabBarIcon: ({ color, size}) => <Ionicons name="home-outline" size={size} color={color}/> }}
+            />      
+        </Tab.Navigator> 
+    )
+}
+
+const AppNavigation = () => {
+  return (
+    <Stack.Screen initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}></Stack.Screen>
+        <Stack.Screen name="Main" component={MainTabs} options={{headerShown: false}}></Stack.Screen>
+        <Stack.Screen name="Datails" component={DatailsScreen}></Stack.Screen>
+    </Stack.Screen>
+  )
+}
+
+export default AppNavigation
