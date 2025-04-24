45f0c18f24a1d010a99010ab12c32586352f7ff1
EdilsonMaria <edilson.eng1@gmail.com>
2025-04-24 17:38:30 -0300

    ajustes dos .env do firebaseConfig

--- aula03/src/config/firebaseConfig.js
+++ aula03/src/config/firebaseConfig.js
@@ -1,16 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID
+} from '@env'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCWkRG-qWFHP1X6dMQTyK_Vrf-6fXZIvcc",
-  authDomain: "tech-cursos-edilson.firebaseapp.com",
-  projectId: "tech-cursos-edilson",
-  storageBucket: "tech-cursos-edilson.firebasestorage.app",
-  messagingSenderId: "842016316992",
-  appId: "1:842016316992:web:6f4cc9e17f6ba54a6009a5"
+  apiKey:  API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
