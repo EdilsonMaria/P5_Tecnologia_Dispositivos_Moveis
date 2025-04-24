a155b43100300a3be9064ce1d8b724dc17d6704c
EdilsonMaria <edilson.eng1@gmail.com>
2025-04-24 17:45:13 -0300

    ajuste do firebaseConfig

--- aula03/src/config/firebaseConfig.js
+++ aula03/src/config/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import {
   API_KEY,
   AUTH_DOMAIN,
@@ -20,3 +21,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export { db }
